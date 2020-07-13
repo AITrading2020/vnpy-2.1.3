a932b1efd3897948b0a0f1362d6dfb3192828495
linfenglee <lilinfeng_cn@163.com>
2020-07-13 21:09:24 +0800

    Scalable for all China option markets

--- vnpy/app/portfolio_strategy/option_backtesting.py
+++ vnpy/app/portfolio_strategy/option_backtesting.py
@@ -5,8 +5,7 @@ from functools import lru_cache
 import traceback
 import pandas as pd
 import re
-from .data_import import DataImport
-#from option_check import OptionCheck
+from .option_data import OptionDataImport, DataImport
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,9 +24,9 @@ from .backtesting import BacktestingEngine, ContractDailyResult, PortfolioDailyR
 sns.set_style("whitegrid")
 
 INTERVAL_DELTA_MAP = {
-    Interval.MINUTE: timedelta(minutes=1),
-    Interval.HOUR: timedelta(hours=1),
-    Interval.DAILY: timedelta(days=1),
+	Interval.MINUTE: timedelta(minutes=1),
+	Interval.HOUR: timedelta(hours=1),
+	Interval.DAILY: timedelta(days=1),
 }
 
 
@@ -36,20 +35,57 @@ class OptionBacktestingEngine(BacktestingEngine):
 
 	gateway_name = "OPTION_BACKTESTING"
 
-	def __init__(self, exchange_days, posit, rate, slippage, size, pricetick):
+	def __init__(self, underlying, exchange_days, posit, rate, slippage, size, pricetick):
 		""""""
 		super().__init__()
 		self.posit = posit
+
+		option_exchange_dict = {
+			"510050": "SHFE", "510300": "SHFE",
+			"CU": "SHFE", "AU": "SHFE", "RU": "SHFE",
+			"C": "DCE", "M": "DCE", "I": "DCE", "PG": "DCE",
+			"MA": "CFFEX", "CF": "CFFEX", "RM": "CFFEX", "SR": "CFFEX", "TA": "CFFEX"
+		}
 
 		self.rate = rate
 		self.slippage = slippage
 		self.size = size
 		self.pricetick = pricetick
 		self.pre_opt_symbols = []
-		self.opt_info = DataImport(exchange_days)
-		self.pre_opt_symbols = []
-		self.opt_info = DataImport(exchange_days)
+		self.underlying = underlying
+		self.option_exchange = option_exchange_dict[underlying]
+		# self.opt_info = OptionDataImport(exchange_days, underlying)
+		self.opt_info = DataImport(exchange_days, underlying)
 
+	def set_parameters(
+		self,
+		vt_symbols: List[str],
+		interval: Interval,
+		start: datetime,
+		rates: Dict[str, float],
+		slippages: Dict[str, float],
+		sizes: Dict[str, float],
+		priceticks: Dict[str, float],
+		capital: int = 0,
+		end: datetime = None
+	) -> None:
+		""""""
+		self.vt_symbols = vt_symbols
+		self.interval = interval
+
+		self.rates = rates
+		self.slippages = slippages
+		self.sizes = sizes
+		self.priceticks = priceticks
+
+		self.start = start
+		self.end = end
+		self.capital = capital
+
+		# add basic vt symbols
+		self.basic_vt_symbols = vt_symbols
+
+	# override the load_data() in backtesting
 	def load_data(self) -> None:
 		""""""
 		self.output("开始加载历史数据")
@@ -103,32 +139,32 @@ class OptionBacktestingEngine(BacktestingEngine):
 
 		self.output("所有历史数据加载完成")
 
-# load option data on_bars() function
-    def load_data_new_bars(self, opt_symbols):
-        self.output("开始加载历史数据")
+	# load option data on_bars() function
+	def load_data_new_bars(self, opt_symbols):
+		self.output("开始加载历史数据")
 
-        if not self.end:
-            self.end = datetime.now()
+		if not self.end:
+			self.end = datetime.now()
 
-        if self.start >= self.end:
-            self.output("起始日期必须小于结束日期")
-            return
+		if self.start >= self.end:
+			self.output("起始日期必须小于结束日期")
+			return
 
-        # Clear previously loaded history data
-        #self.history_data.clear()
-        #self.dts.clear()
+		# Clear previously loaded history data
+		# self.history_data.clear()
+		# self.dts.clear()
 
-        # Load 30 days of data each time and allow for progress update
-        progress_delta = timedelta(days=30)
-        total_delta = self.end - self.start
-        interval_delta = INTERVAL_DELTA_MAP[self.interval]
+		# Load 30 days of data each time and allow for progress update
+		progress_delta = timedelta(days=30)
+		total_delta = self.end - self.start
+		interval_delta = INTERVAL_DELTA_MAP[self.interval]
 
-        for vt_symbol in opt_symbols:
-            start = self.start
-            end = self.start + progress_delta
-            progress = 0
+		for vt_symbol in opt_symbols:
+			start = self.start
+			end = self.start + progress_delta
+			progress = 0
 
-            data_count = 0
+			data_count = 0
 			while start < self.end:
 				end = min(end, self.end)  # Make sure end time stays within set range
 
@@ -154,8 +190,6 @@ class OptionBacktestingEngine(BacktestingEngine):
 
 			self.output(f"{vt_symbol}历史数据加载完成，数据量：{data_count}")
 
-			self.output("所有历史数据加载完成")
-
 	def new_bars(self, dt: datetime) -> None:
 		""""""
 		self.datetime = dt
@@ -163,9 +197,9 @@ class OptionBacktestingEngine(BacktestingEngine):
 		self.bars.clear()
 
 		date = pd.to_datetime(str(dt)[:10])
-		tradable_opt, _ = self.opt_info.available_opt(date, self.posit)
-		call_symbol = str(tradable_opt[tradable_opt["option_type"] == "C"]["order_book_id"].values[0]) + ".SHFE"
-		put_symbol = str(tradable_opt[tradable_opt["option_type"] == "P"]["order_book_id"].values[0]) + ".SHFE"
+		tradable_opt, ttm = self.opt_info.available_opt(date, self.posit)
+		call_symbol = str(tradable_opt[tradable_opt["option_type"] == "C"]["order_book_id"].values[0]) + "." + self.option_exchange
+		put_symbol = str(tradable_opt[tradable_opt["option_type"] == "P"]["order_book_id"].values[0]) + "." + self.option_exchange
 		opt_symbols = [call_symbol, put_symbol]
 		new_symbols = list(set(opt_symbols) - set(self.vt_symbols))
 		if len(new_symbols) > 0:
@@ -176,13 +210,13 @@ class OptionBacktestingEngine(BacktestingEngine):
 				self.priceticks[vt_symbol] = self.pricetick
 			self.vt_symbols = self.vt_symbols + new_symbols
 			self.load_data_new_bars(new_symbols)
-		opt_data = DataImport(0)
+		opt_data = DataImport(0, self.underlying)
 		exsit_opt = opt_data.get_opt_info(date)
 		trade_symbols = [trade.vt_symbol for trade in self.trades.values()] + opt_symbols + self.pre_opt_symbols
-		# exist_symbols = list(set(trade_symbols).intersection(set(list(exsit_opt["order_book_id"].apply(lambda x: str(x) + ".SHFE").values))))
 		exist_symbols = list(set(self.vt_symbols).intersection(
-			set(list(exsit_opt["order_book_id"].apply(lambda x: str(x) + ".SHFE").values))))
-		exist_symbols.append("510050.SSE")
+			set(list(exsit_opt["order_book_id"].apply(lambda x: str(x) + "." + self.option_exchange).values))))
+		exist_symbols = exist_symbols + self.basic_vt_symbols
+		# print(exist_symbols)
 		for vt_symbol in exist_symbols:
 			bar = self.history_data.get((dt, vt_symbol), None)
 			if bar:
@@ -193,14 +227,14 @@ class OptionBacktestingEngine(BacktestingEngine):
 
 		self.pre_opt_symbols = opt_symbols
 		self.cross_limit_order()
-		self.strategy.on_bars(self.bars)
+		self.strategy.on_bars(self.bars, tradable_opt, ttm, self.option_exchange)
 
 		self.update_daily_close(self.bars, dt)
 
 	def cross_limit_order(self) -> None:
 		"""
-        Cross limit order with last bar/tick data.
-        """
+		Cross limit order with last bar/tick data.
+		"""
 		for order in list(self.active_limit_orders.values()):
 			bar = self.bars[order.vt_symbol]
 
